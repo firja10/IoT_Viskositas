3d2673d743605e751bd3656fdf8e5e7108b6e6aa
firja10 <fairuzfirjatullah3@gmail.com>
2023-07-03 13:37:28 +0700

    Update

--- app/Http/Controllers/ViskositasController.php
+++ app/Http/Controllers/ViskositasController.php
@@ -184,7 +184,7 @@ class ViskositasController extends Controller
 
 
 
-        return $angkaVoltage;
+        return 'RPM :' . $angkaVoltage .'<br>' . 'Current: ' . $i_new . '<br>' . 'RPM:' . $f; 
 
 
 
